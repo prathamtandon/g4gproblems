c8bbc232ccc42aa255fd921f81d7876bbf1ac908
Pratham Tandon <prathamt@outlook.com>
2016-12-19 17:45:59 +0530

    Breadth first paths

---
+++ Data Structures/breath_first_paths.py
@@ -0,0 +1,30 @@
+class BreadthFirstPaths:
+
+    def __init__(self, G, s):
+        self.marked = [False] * G.V()
+        self.s = s
+        self.edge_to = [0] * G.V()
+        self.bfs(G)
+
+    def bfs(self, G):
+        self.marked[self.s] = True
+        queue = [self.s]
+        self.edge_to[self.s] = self.s
+        while not len(queue) > 0:
+            v = queue.pop(0)
+            for w in G.adj(v):
+                if not self.marked[w]:
+                    self.marked[w] = True
+                    self.edge_to[w] = v
+                    queue.append(w)
+
+    def marked(self, v):
+        return self.marked[v]
+
+    def path_to(self, v):
+        x = self.edge_to[v]
+        path = [v]
+        while x != self.s:
+            path.insert(0, x)
+            x = self.edge_to[x]
+        return path
