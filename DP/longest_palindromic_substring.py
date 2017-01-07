7c0b4586a0a55c24123badd94a1d6f18b25737b1
Pratham Tandon <prathamt@outlook.com>
2017-01-07 18:59:55 +0530

    Longest palindromic substring

---
+++ DP/longest_palindromic_substring.py
@@ -0,0 +1,38 @@
+import unittest
+"""
+Given a string, find the longest substring which is a palindrome.
+Input: forgeeksskeegfor
+Output: geeksskeeg
+"""
+
+
+def longest_palindromic_substring(string):
+    n = len(string)
+    table = [[0] * n for _ in range(n)]
+    for i in range(n):
+        table[i][i] = 1
+
+    for l in range(2, n+1):
+        for i in range(n-l+1):
+            j = i+l-1
+            if string[i] == string[j] and l == 2:
+                table[i][j] = 2
+            elif string[i] == string[j]:
+                table[i][j] = 2 + table[i+1][j-1]
+
+    start = 0
+    max_length = 0
+    for i in range(n):
+        for j in range(n):
+            if max_length < table[i][j]:
+                max_length = table[i][j]
+                start = i
+
+    return string[start:start+max_length]
+
+
+class TestLongestPalindromic(unittest.TestCase):
+
+    def test_longest_palindromic(self):
+        string = 'forgeeksskeegfor'
+        self.assertEqual(longest_palindromic_substring(string), 'geeksskeeg')
