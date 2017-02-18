ab7bf183cb30f2ba5407721e08dbf21ee6657bc5
Pratham Tandon <prathamt@outlook.com>
2017-02-18 19:31:56 +0530

    Some refactoring

--- DP/longest_palindromic_subsequence.py
+++ DP/longest_palindromic_subsequence.py
@@ -12,13 +12,12 @@ def longest_palindromic_subsequence(string):
     #  table[i][j] is longest palindromic subsequence length for string[i...j]
     table = [[0] * n for _ in range(n)]
 
-    for i in range(n):
-        table[i][i] = 1
-
-    for L in range(2, n+1):
-        for i in range(0, n-L+1):
+    for L in range(1, n+1):
+        for i in range(n-L+1):
             j = i+L-1
-            if string[i] == string[j] and L == 2:
+            if i == j:
+                table[i][j] = 1
+            elif string[i] == string[j] and L == 2:
                 table[i][j] = 2
             elif string[i] == string[j]:
                 table[i][j] = 2 + table[i+1][j-1]
@@ -33,3 +32,4 @@ class TestLPSS(unittest.TestCase):
     def test_lpss(self):
         string = 'GEEKS'
         self.assertEqual(longest_palindromic_subsequence(string), 2)
+        self.assertEqual(longest_palindromic_subsequence('BBABCBCAB'), 7)
