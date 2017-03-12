8f2b9e2f64b903d2319a2a14979df5715d5401f7
Pratham Tandon <prathamt@outlook.com>
2017-03-12 15:32:11 +0530

    Pairwise swap

---
+++ Linked List/pairwise_swap.py
@@ -0,0 +1,58 @@
+"""
+Given a singly linked list, write a function to swap elements pairwise.
+Input: 1->2->3->4->5->6->7
+Output: 2->1->4->3->6->5->7
+Input: 1->2->3->4
+Output: 2->1->4->3
+"""
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def pairwise_swap(head):
+    if not head or not head.next:
+        return head
+
+    prev = head
+    cur = head.next
+
+    head = cur
+
+    while True:
+        temp = cur.next
+        cur.next = prev
+
+        if not temp or not temp.next:
+            prev.next = temp
+            break
+
+        prev.next = temp.next
+        prev = temp
+        cur = prev.next
+
+    return head
+
+
+def print_ll(head):
+    result = []
+    current = head
+    while current:
+        result.append(current.data)
+        current = current.next
+    print result
+
+
+if __name__ == '__main__':
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+
+    print_ll(head)
+    print_ll(pairwise_swap(head))
