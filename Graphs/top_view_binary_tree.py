6de9e32d2d156b20d34a41f11f251cf0226c7c8d
Pratham Tandon <prathamt@outlook.com>
2017-03-13 18:01:28 +0530

    Placeholder

---
+++ Graphs/top_view_binary_tree.py
@@ -0,0 +1,13 @@
+"""
+Top view of binary tree is the set of nodes visible when the tree is viewed from the top.
+Given a binary tree, print the top view of it. Expected time complexity is O(n). The output nodes
+can be printed in any order.
+Input:
+     1
+   /  \
+  2    3
+ /\   /\
+4 5  6 7
+
+Output: 4 2 1 3 7
+"""
