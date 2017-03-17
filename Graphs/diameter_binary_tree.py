0cef924a9df75bded7fdff7d897b47b73224a6e3
Pratham Tandon <prathamt@outlook.com>
2017-03-17 13:42:09 +0530

    Refactor

--- Graphs/diameter_binary_tree.py
+++ Graphs/diameter_binary_tree.py
@@ -1,29 +1,44 @@
 import unittest
 """
-The diameter of a tree is the number of nodes on the longest path between two leaves in the tree.
+Diameter of a binary tree is number of nodes on longest path between any two leaves in the tree.
+Recursive definition of diameter =
+max(diameter of left subtree, diameter of right subtree, 1 + height of left subtree + height of right subtree).
+The first two cases are when diameter path does not include current node and third case is when diameter path
+includes current node.
 """
 
 
 class Node:
-    def __init__(self, key, left=None, right=None):
-        self.key = key
+    def __init__(self, data, left=None, right=None):
+        self.data = data
         self.left = left
         self.right = right
-
-"""
-Diameter of root = max(diameter of root.left, diameter of root.right, 1+height(root.left)+height(root.right))
-"""
+        self.height_of_subtree = 0
 
 
-def diameter(root):
-    if root is None:
-        return 0, 0
-    ldiam, lheight = diameter(root.left)
-    rdiam, rheight = diameter(root.right)
-    return max(ldiam, rdiam, 1+lheight+rheight), 1+max(lheight, rheight)
+class BinaryTree:
+    def __init__(self, root):
+        self.root = root
+
+    def diameter(self):
+        return self.diameter_of_subtree(self.root)
+
+    def diameter_of_subtree(self, node):
+        if node is None:
+            return -float('inf')
+
+        left_diameter = self.diameter_of_subtree(node.left)
+        right_diameter = self.diameter_of_subtree(node.right)
+
+        left_height = node.left.height_of_subtree if node.left else 0
+        right_height = node.right.height_of_subtree if node.right else 0
+
+        node.height_of_subtree = 1 + max(left_height, right_height)
+
+        return max(left_diameter, right_diameter, 1 + left_height + right_height)
 
 
-class TestDiameter(unittest.TestCase):
+class TestBinaryTree(unittest.TestCase):
 
     def test_diameter(self):
         root = Node(1)
@@ -31,4 +46,6 @@ class TestDiameter(unittest.TestCase):
         root.right = Node(3)
         root.left.left = Node(4)
         root.left.right = Node(5)
-        self.assertEqual(diameter(root)[0], 4)
+
+        binary_tree = BinaryTree(root)
+        self.assertEqual(binary_tree.diameter(), 4)
