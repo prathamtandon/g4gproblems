fa9ebc9cb56ab393a00af1c2d029553ff64e27e0
Pratham Tandon <prathamt@outlook.com>
2017-07-30 11:44:09 +0530

    kadane maximum sum subarray DP version

--- Arrays/kadane_max_sum_subarray.py
+++ Arrays/kadane_max_sum_subarray.py
@@ -1,19 +1,21 @@
 def kadane_max_sum_subarray(arr):
-
+    n = len(arr)
+    # dp[i] denotes the max sum value for arr[0...i]
+    # At each element, we decide either to leverage to running sum so far, or start a new subarray.
+    dp = [0] * n
     sumVal = 0
-    ans = -float('inf')
-
-    for i in range(len(arr)):
+    for i in range(n):
         sumVal += arr[i]
-        ans = max(ans, sumVal)
-        if sumVal < 0:
-            sumVal = 0
+        dp[i] = max(sumVal, arr[i])
+        sumVal = max(sumVal, 0)
 
-    return ans
+    return max(dp)
 
 
 if __name__ == '__main__':
     arr = [2, 3, -5, 9]
     print kadane_max_sum_subarray(arr)
     arr = [-4, -5, -2, -7]
+    print kadane_max_sum_subarray(arr)
+    arr = [-2, -3, 4, -1, -2, 1, 5, -3]
     print kadane_max_sum_subarray(arr)
