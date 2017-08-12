2071600ac56592fc2661e4d21132ad075aae8cd7
Pratham Tandon <prathamt@outlook.com>
2017-08-12 18:38:50 +0530

    Added approach explanation

--- Sorting and Searching/sort_almost_sorted.py
+++ Sorting and Searching/sort_almost_sorted.py
@@ -1,21 +1,28 @@
 """
-Given an almost sorted array where only two elements are swapped, how to sort the array efficiently.
+Given an almost sorted array in ascending order where only two elements are swapped,
+how to sort the array efficiently.
+"""
+
+"""
+Approach:
+1. Scan from left to right to find the first larger element on the left side ie x[i]>x[i+1]
+2. Scan from n-1 down to i, and find the first j such that x[j]<=x[i+1]
+3. Swap x[i] and x[j]
 """
 
 
-def sort_almost_sorted(arr):
-    n = len(arr)
+def sort_almost_sorted(x):
+    n = len(x)
 
-    for i in range(n-1, 0, -1):
-        if arr[i] < arr[i-1]:
-            j = i - 1
-            while j >= 0 and arr[j] > arr[i]:
-                j -= 1
-            arr[i], arr[j+1] = arr[j+1], arr[i]
-            break
+    for i in range(n - 1):
+        if x[i] > x[i + 1]:
+            for j in range(n - 1, i, -1):
+                if x[j] <= x[i + 1]:
+                    x[i], x[j] = x[j], x[i]
+                    return
 
 
 if __name__ == '__main__':
-    arr = [1, 5, 3]
+    arr = [1, 44, 21, 33, 12, 90]
     sort_almost_sorted(arr)
     print arr
